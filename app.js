98a542cc980ac29d765b6b331078e4070f124f37
Sandipan Pramanik <sandipan.pramanik@ibm.com>
2022-06-15 02:42:14 +0530

    added 1 new GET API

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-/**
-* Copyright 2019 IBM
-*
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-**/
 
 var express = require('express');
 const message = require('./utils');
@@ -29,29 +14,8 @@ app.get('/', function (req, res) {
   res.send(message.getWelcomeMessage());
 });
 
-  
-// Defining get request at '/single' route
-app.get('/single', function(req, res) {
-  res.json({
-    id: 1,
-    firstName: 'Sandipan',
-    lastName: 'Pramanik'
-  });
-});
-  
-// Defining get request at '/list' route
-app.get('/list', function(req, res) {
-  res.json([{
-        id: 1,
-        firstName: 'Sandipan',
-        lastName: 'Pramanik'
-    },
-    {
-        id: 2,
-        firstName: 'Adrish',
-        lastName: 'Pramanik'
-    }
-  ]);
+app.get('/employees', function(req, res) {
+  res.send(employeesList);
 });
 
 app.get('/employees/:id', function(req, res) {
@@ -60,6 +24,7 @@ app.get('/employees/:id', function(req, res) {
   var empFound = employeesList.filter(function(employee) {
     return employee.id == empId;
   });
+
   res.send(empFound);	
 });
 
